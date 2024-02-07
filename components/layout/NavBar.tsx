50ed6738bacc1c24b7511f8d50960f8ffc7fac78
Burak Erçıktı <burakercikti@outlook.com>
2024-02-07 11:06:50 +0300

    Authentication Edited

---
+++ components/layout/NavBar.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { UserButton } from "@clerk/nextjs";
+
+const NavBar = () => {
+  return (
+    <div>
+        <UserButton afterSignOutUrl="/"/>   
+    </div>
+  )
+}
+
+export default NavBar
